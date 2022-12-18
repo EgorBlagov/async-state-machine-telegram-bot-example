d5bb2d48a6937de8880b17775d6f48c18d8429f4
Egor Blagov <e.m.blagov@gmail.com>
2022-12-18 20:06:25 +0400

    Make everythin async

--- main.py
+++ main.py
@@ -3,45 +3,46 @@ import asyncio
 import abc
 import aiohttp
 import yarl
-import requests
 
 
-def simple_request(url: str, **query_params: Any) -> dict:
-    return requests.get(yarl.URL(url).update_query(**query_params)).json()
+async def simple_request(url: str, **query_params: Any) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(yarl.URL(url).update_query(**query_params)) as response:
+            return await response.json()
 
 
 class IoApi(abc.ABC):
     @abc.abstractmethod
-    def input(self, prompt: str | None = None) -> str:
+    async def input(self, prompt: str | None = None) -> str:
         pass
 
     @abc.abstractmethod
-    def print(self, message: str) -> None:
+    async def print(self, message: str) -> None:
         pass
 
     @abc.abstractmethod
-    def choose(self, *options: str) -> str:
+    async def choose(self, *options: str) -> str:
         pass
 
 
 class CliApi(IoApi):
-    def input(self, prompt: str | None = None) -> str:
+    async def input(self, prompt: str | None = None) -> str:
         return input(prompt)
 
-    def print(self, message: str) -> None:
+    async def print(self, message: str) -> None:
         print(message)
 
-    def choose(self, *options: str) -> str:
+    async def choose(self, *options: str) -> str:
         while True:
             try:
                 option_lines = [f"{i}) {opt}" for i, opt in enumerate(options)]
-                self.print("\n".join(option_lines))
-                user_input = self.input("Enter index: ")
+                await self.print("\n".join(option_lines))
+                user_input = await self.input("Enter index: ")
                 return options[int(user_input)]
             except KeyboardInterrupt:
                 raise
             except:
-                self.print("Try again")
+                await self.print("Try again")
 
 
 class State(abc.ABC):
@@ -49,24 +50,24 @@ class State(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def run(self, io: IoApi) -> "State":
+    async def run(self, io: IoApi) -> "State":
         pass
 
 
 class FindCityPosition(State):
-    def run(self, io: IoApi) -> "State":
-        city = io.input("Name a city: ")
-        response = simple_request(
+    async def run(self, io: IoApi) -> "State":
+        city = await io.input("Name a city: ")
+        response = await simple_request(
             "https://geocoding-api.open-meteo.com/v1/search", name=city
         )
         cities = response.get("results")
 
         if not cities:
-            io.print("Found nothing")
+            await io.print("Found nothing")
             return self
 
         city = cities[0]
-        io.print(
+        await io.print(
             "Found {name} ({country}) at latitude {latitude} and longitude {longitude}".format(
                 **city
             )
@@ -80,15 +81,15 @@ class ShowWeatherAtCoord(State):
         self.latitude = latitude
         self.longitude = longitude
 
-    def run(self, io: IoApi) -> "State":
-        response = simple_request(
+    async def run(self, io: IoApi) -> "State":
+        response = await simple_request(
             "https://api.open-meteo.com/v1/forecast",
             latitude=self.latitude,
             longitude=self.longitude,
             current_weather=1,
         )
 
-        io.print(
+        await io.print(
             f"Current temperature is {response['current_weather']['temperature']} °C"
         )
 
@@ -98,26 +99,27 @@ class ShowWeatherAtCoord(State):
 class ExitOrContinue(State):
     CONTINUE, QUIT = "continue", "quit"
 
-    def run(self, io: IoApi) -> "State":
-        choice = io.choose(self.CONTINUE, self.QUIT)
+    async def run(self, io: IoApi) -> "State":
+        choice = await io.choose(self.CONTINUE, self.QUIT)
         if choice == self.QUIT:
             raise State.ExitLoop
 
         return FindCityPosition()
 
 
-def weather_query(io: IoApi):
+async def weather_query(io: IoApi):
     current_state = FindCityPosition()
 
     while True:
         try:
-            next_state = current_state.run(io)
+            next_state = await current_state.run(io)
             current_state = next_state
 
         except State.ExitLoop:
-            io.print("Terminating")
+            await io.print("Terminating")
             return
 
 
 io = CliApi()
-weather_query(io)
+loop = asyncio.new_event_loop()
+loop.run_until_complete(weather_query(io))
